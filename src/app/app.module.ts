f919180da126bee3794b110ae49b732e21c3d6d6
yashtongia21 <tongia.yash21@gmail.com>
2018-10-22 18:05:45 +0530

    feedback-update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
 
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
@@ -11,13 +12,16 @@ import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { PlanFormComponent } from './component/plan-form/plan-form.component';
 import { PlanDetailsComponent } from './component/plan-details/plan-details.component';
 import { HomeComponent } from './component/home/home.component';
-
+import { FeedbackformComponent } from './component/feedbackform/feedbackform.component';
+import { FeedbackpageComponent } from './component/feedbackpage/feedbackpage.component';
 
 const paths: Routes = [
   { path: '', component: HomeComponent },
   { path: 'listPlans', component: DisplayPlansComponent },
   { path: 'planForm', component: PlanFormComponent },
-  {path:'viewPlan/:id',component:PlanDetailsComponent}
+  {path:'viewPlan/:id',component:PlanDetailsComponent},
+  { path: 'feedback-form', component: FeedbackformComponent },
+  { path: 'successful/:id', component: FeedbackpageComponent }
   
 ];
 
@@ -28,13 +32,16 @@ const paths: Routes = [
     DashboardComponent,
     PlanFormComponent,
     PlanDetailsComponent,
-    HomeComponent
+    HomeComponent,
+    FeedbackformComponent,
+    FeedbackpageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(paths),
+    AngularFontAwesomeModule,
+    RouterModule.forRoot(paths)
    
   ],
   providers: [],
